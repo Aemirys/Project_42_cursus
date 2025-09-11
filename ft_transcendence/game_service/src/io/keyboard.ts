b4c877be561514880f380916f9405a35b0a595a0
Emilie STOSCHEK <148214660+Aemirys@users.noreply.github.com>
2025-09-11 14:09:11 +0200

    Delete ft_transcendence/game_service directory

--- ft_transcendence/game_service/src/io/keyboard.ts
+++
@@ -1,7 +0,0 @@
-const keys: Record<string, boolean> = {};
-window.addEventListener('keydown', e => (keys[e.key] = true));
-window.addEventListener('keyup',   e => (keys[e.key] = false));
-
-export function pressed(key: string): boolean {
-  return !!keys[key];
-}
